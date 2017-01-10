c166fec5f5f4b6159abe28447c6ac36f6a439ae3
Joao Gabriel <joaogfconc@gmail.com>
2017-01-09 22:11:34 -0200

    testando o retrofit

---
+++ app/src/main/java/br/com/monitoratec/app/domain/GitHubStatusApi.java
@@ -0,0 +1,15 @@
+package br.com.monitoratec.app.domain;
+
+import br.com.monitoratec.app.domain.entity.Status;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by joaog on 09/01/2017.
+ */
+
+public interface GitHubStatusApi {
+    String BASE_URL = "https://status.github.com/api/";
+    @GET("last-message.json")
+    Call<Status> lastMessage();
+}
---
+++ app/src/main/java/br/com/monitoratec/app/domain/entity/Status.java
@@ -0,0 +1,11 @@
+package br.com.monitoratec.app.domain.entity;
+
+/**
+ * Created by joaog on 09/01/2017.
+ */
+
+public class Status {
+    public String status;
+    public String body;
+    public String created_on;
+}
